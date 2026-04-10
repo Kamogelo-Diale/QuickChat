658be61aab3b325deb57a382add5b7c2855a0ffb
Student <Student@LabVM2047644.5ky0irqfkqgebhftalxgpazyba.jnbx.internal.cloudapp.net>
2026-04-10 11:37:21 +0200

    added boolean for checkUserName, checkPassword and checkCellphone

--- src/quickchat/QuickChat.java
+++ src/quickchat/QuickChat.java
@@ -5,7 +5,7 @@
 package quickchat;
 import java.util.Scanner;
 /**
- *
+ * //("^+27\\d{9}")
  * @author Student
  */
 class Login {
@@ -13,6 +13,37 @@ class Login {
     String Password;
     String Cellphone;
     
+    boolean checkUsername (String Username){
+     if (Username.length()== 5 && Username.contains("_")){
+         System.out.println("Username successfully captured");
+     return true;
+         
+     } 
+     else{
+         System.out.println("Username is not correctly");
+         return false;
+     }
+    }
+    boolean checkPassword(String Password){
+        if (Password.length()==8 && Password.contains(".[!@#$%^&*].*") ){
+         System.out.println("Password captured successfully");
+         return true;
+        }
+        else{
+            System.out.println("Password is not correctly formated");
+            return false;
+        }
+    }
+    boolean checkCellphone (String Cellphone){
+        if (Cellphone.length()==10 && Cellphone.contains("//^+27\\d(9)")){
+        System.out.println("Cellphone number successfully added");
+        return true;
+        }
+        else{
+            System.out.println("Cellphone number incorrectly or does not contain intentional code");
+            return false;
+        }
+    }
 }
 
 public class QuickChat {
