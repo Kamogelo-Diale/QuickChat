8271d235acad5762e7e92768e046680fdd0446f2
Student <Student@LabVM2047644.5ky0irqfkqgebhftalxgpazyba.jnbx.internal.cloudapp.net>
2026-04-10 09:50:56 +0200

    added String UserName, Password and cellphone

--- src/quickchat/QuickChat.java
+++ src/quickchat/QuickChat.java
@@ -3,11 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package quickchat;
-import java.util.*;
+import java.util.Scanner;
 /**
  *
  * @author Student
  */
+class Login {
+    String Username;
+    String Password;
+    String Cellphone;
+    
+}
+
 public class QuickChat {
 
     /**
@@ -15,7 +22,7 @@ public class QuickChat {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String Name;
+        
     }
     
 }
